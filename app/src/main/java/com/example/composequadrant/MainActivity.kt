59322610bfe9729007798f32255b5f72342e7b3e
Alexander <alexenter@gmail.com>
2025-06-15 18:48:48 -0500

    Text to string resources

---
+++ app/src/main/java/com/example/composequadrant/MainActivity.kt
@@ -0,0 +1,154 @@
+package com.example.composequadrant
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ComposeQuadrantTheme()
+        }
+    }
+}
+
+@Composable
+fun ComposeQuadrantTheme() {
+    Column(Modifier.fillMaxSize()) {
+        // Primera fila (dos cuadrantes superiores)
+        Row(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxSize()
+        ) {
+            Quadrant(
+                title = stringResource(R.string.text_composable),
+                description = stringResource(R.string.text_composable_description),
+                backgroundColor = Color(0xFFEADDFF),
+                modifier = Modifier.weight(1f)
+            )
+            QuadrantImg(
+                title = stringResource(R.string.image_composable),
+                description = stringResource(R.string.image_composable_description),
+                backgroundColor = Color(0xFFD0BCFF),
+                modifier = Modifier.weight(1f)
+            )
+        }
+        // Segunda fila (dos cuadrantes inferiores)
+        Row(
+            modifier = Modifier
+                .weight(1f) // Ocupa la otra mitad del alto disponible en la Column
+                .fillMaxSize()
+        ) {
+            Quadrant(
+                title = stringResource(R.string.row_composable),
+                description = stringResource(R.string.row_composable_description),
+                backgroundColor = Color(0xFFB69DF8),
+                modifier = Modifier.weight(1f) // Ocupa la mitad del ancho disponible en la Row
+            )
+            Quadrant(
+                title = stringResource(R.string.column_composable),
+                description = stringResource(R.string.column_composable_description),
+                backgroundColor = Color(0xFFF6EDF),
+                modifier = Modifier.weight(1f) // Ocupa la mitad del ancho disponible en la Row
+            )
+        }
+    }
+}
+
+@Composable
+fun QuadrantImg(
+    title: String,
+    description: String,
+    backgroundColor: Color,
+    modifier: Modifier = Modifier
+) {
+    val image = painterResource(id = R.drawable.arte_y_confort)
+    Column(
+        modifier = modifier
+            .background(backgroundColor)
+            .padding(16.dp)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = title,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+        Image(
+            painter = image,
+            contentDescription = null,
+            modifier = Modifier
+                .padding(bottom = 8.dp),
+            contentScale = ContentScale.Crop
+        )
+        Text(
+            text = description,
+            textAlign = TextAlign.Justify
+            )
+    }
+
+
+}
+
+@Composable
+fun Quadrant(
+    title: String,
+    description: String,
+    backgroundColor: Color,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .background(backgroundColor)
+            .padding(16.dp)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = title,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+        Text(
+            text = description,
+            textAlign = TextAlign.Justify,
+
+        )
+    }
+
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ComposeQuadrantAppPreview() {
+    ComposeQuadrantTheme()
+}
