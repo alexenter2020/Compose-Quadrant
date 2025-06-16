3f675bf25f5e169ea9eae1d19577162d53b4cb99
Alexander <alexenter@gmail.com>
2025-06-15 20:38:53 -0500

    Adaptado a almacén de muebles, con imagen en un cuadrante. Adapted to a furniture warehouse, with an image in a quadrant.

--- app/src/main/java/com/example/composequadrant/MainActivity.kt
+++ app/src/main/java/com/example/composequadrant/MainActivity.kt
@@ -10,19 +10,19 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.stringResource
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -104,8 +104,9 @@ fun QuadrantImg(
             painter = image,
             contentDescription = null,
             modifier = Modifier
-                .padding(bottom = 8.dp),
-            contentScale = ContentScale.Crop
+                .padding(bottom = 8.dp)
+                .fillMaxWidth()
+
         )
         Text(
             text = description,
